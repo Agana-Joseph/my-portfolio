32aafc0fc801a3dce571850494e6304c02b22126
Agana-Joseph <josephagana8@gmail.com>
2023-10-02 03:10:43 +0100

    Portfolio

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <link href="/portfolio">
+            <a>Product</a>
+            </link>
+        </div>
+    )
+}
+
+export default Home
